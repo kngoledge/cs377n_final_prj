96bcb553b97160a50aa1284dd7111d89e270ce5b
czarevalo <cesarja@stanford.edu>
2020-06-06 14:32:53 -0700

    Add files via upload

--- config.h
+++ config.h
@@ -4,3 +4,20 @@ const char* wifi_ssid = "Mirada672";  // network name -- put your WiFi SSID here
 const char* wifi_password = "672261612";     // network password -- enter your WiFi password (if one is needed)
 
 /************************ Server Config *******************************/
+
+const String io_key = "aio_yklC18ji3hu0qd05FOmdAJWAzVlq";  // YOUR ADAFRUIT IO KEY - YOU MUST REPLACE
+const String io_host = "io.adafruit.com";
+const int io_port = 443;
+const String io_username = "kimanh";          
+const String io_feedkey = "activity";         
+const String post_io_feed_path = "/api/v2/" + io_username + "/feeds/" + io_feedkey + "/data";
+const String get_io_feed_path = "/api/v2/" + io_username + "/feeds/" + io_feedkey + "/data/retain";
+
+/************************ Server Config 2 *******************************/
+
+const String io_key2 = "aio_wwbq90YVVpkadpQxzhqOBrZ8DeLI";  // YOUR ADAFRUIT IO KEY - YOU MUST REPLACE
+const String post_io_feed_path2 = "/api/v2/" + io_username + "/feeds/" + io_feedkey + "/data";
+const String get_io_feed_path2 = "/api/v2/" + io_username + "/feeds/" + io_feedkey + "/data/retain";
+
+/************************ Your Name *******************************/
+const String your_name = "Cesar";
--- distantSocializing.ino
+++ distantSocializing.ino
@@ -2,8 +2,7 @@
    ME216M Final Project: Distant Socializing
    By: Cesar Arevalo & Kim Ngo
    Last Updated: 5/26/20
-   Inputs: ultrasonic sensor, light sensor,
-   4x buttons
+   Inputs:light sensor, 4x buttons
    Outputs: OLED Screen, LED array
    Summary: This program monitors and responds
    to events.
@@ -40,7 +39,17 @@
 //         Wireless Setup
 //---------------------------------
 WiFiSSLClient wifi;
-//HttpClient GetClient = HttpClient(wifi, ow_host, ow_port);
+// GetClient and PostClient access the same feed, therefore have the same information
+HttpClient GetClient = HttpClient(wifi, io_host, io_port);
+HttpClient PostClient = HttpClient(wifi, io_host, io_port);
+
+// Strings to GET and POST
+String phrase;
+String getMessage;
+String postMessage;
+String messages[7];
+int phraseNum;
+unsigned long AWAITING_MESSAGE_DURATION = 10000; // 10 seconds
 
 //---------------------------------
 //         OLED Variables
@@ -54,10 +63,11 @@ U8G2_SH1106_128X64_NONAME_F_HW_I2C oled(U8G2_R0, U8X8_PIN_NONE);
 //  Event & State Variables
 //--------------------------
 
-#define LIGHT_SENSOR_PIN       A0 // put sensor on an analog pin
+#define LIGHT_SENSOR       A0 // put sensor on an analog pin
 #define LEFT_BUTTON             2
 #define CENTER_BUTTON           4
 #define RIGHT_BUTTON            6
+#define SELECT_BUTTON          16
 
 
 #define LIGHT_THRESHOLD_LOW 350
@@ -66,10 +76,14 @@ unsigned long WAVE_DURATION = 200;
 unsigned long BUTTON_DURATION = 100;
 unsigned long waveTimer;
 unsigned long buttonTimer;
+unsigned long messageTimer;
 
-//Events Defitions
-#define EVENT_LIGHT  EventManager::kEventUser0
-#define EVENT_BUTTON EventManager::kEventUser1
+//Events Definitions
+#define EVENT_LIGHT   EventManager::kEventUser0
+#define EVENT_BUTTON  EventManager::kEventUser1
+#define EVENT_MESSAGE EventManager::kEventUser2
+#define EVENT_SELECT  EventManager::kEventUser3
+#define EVENT_DECODED EventManager::kEventUser4
 
 //Create the Event Manager
 EventManager eventManager;
@@ -82,17 +96,22 @@ SystemState_t currentState = INIT;
 
 void setup() {
   Serial.begin(9600); //initialize serial
-  //setupWifi();
+  while (!Serial && millis() < 6000); // wait for serial to connect (6s timeout)
+  setupWifi();
   //setupOLED();
 
   eventManager.addListener(EVENT_LIGHT, stateMachine);
   eventManager.addListener(EVENT_BUTTON, stateMachine);
+  eventManager.addListener(EVENT_MESSAGE, stateMachine);
+  eventManager.addListener(EVENT_SELECT, stateMachine);
+  eventManager.addListener(EVENT_DECODED, stateMachine);
 
   while (!Serial); //wait for serial to connect
 
   stateMachine(INIT, 0);
   waveTimer = millis();
   buttonTimer = millis();
+  messageTimer = millis();
 }
 
 void loop() {
@@ -100,12 +119,64 @@ void loop() {
   checkEvents();
 }
 
+void buildPhrases() {
+  String phraseOne    = "I miss you!";
+  String phraseTwo    = "How are you?";
+  String phraseThree  = "I'm doing well!";
+  String phraseFour   = "Ok!";
+  String phraseFive   = "Message 1";
+  String phraseSix    = "Message 2";
+  String phraseSeven  = "Message 3";
+
+  messages[0] = phraseOne;
+  messages[1] = phraseTwo;
+  messages[2] = phraseThree;
+  messages[3] = phraseFour;
+  messages[4] = phraseFive;
+  messages[5] = phraseSix;
+  messages[6] = phraseSeven;
+}
+
 void checkEvents() {
+  checkButtons();
+  checkSelect();
   if (millis() - waveTimer > WAVE_DURATION) {
     checkLight();
     waveTimer = millis(); // reset timer
   }
-  checkButtons();
+  if (millis() - messageTimer > AWAITING_MESSAGE_DURATION) {
+    checkMessages();
+    messageTimer = millis(); // reset timer
+  }
+}
+
+void checkSelect() {
+  bool eventHappened = false;  // Initialize our eventHappened flag to false
+  int eventParameter = 0;       // Change type as needed
+
+  // Create variable to hold last button pin reading
+  // note: static variables are only initialized once and keep their value between function calls.
+  static int lastSelectButton = LOW;
+
+  // Read value of button pin
+  int thisSelectButton = digitalRead(SELECT_BUTTON);
+
+  // Check if button was pressed
+  if (thisSelectButton != lastSelectButton) {
+    if (thisSelectButton == HIGH) {
+      // Set flag so event will be posted
+      eventHappened = true;
+    }
+  }
+
+  // Post the event if it occured
+  if (eventHappened == true) {
+    eventManager.queueEvent(EVENT_SELECT, eventParameter);
+  }
+
+  // Update last button reading to current reading
+  lastSelectButton = thisSelectButton;
+
 }
 
 void checkLight() {
@@ -116,7 +187,7 @@ void checkLight() {
   // note: static variables are only initialized once and keep their value between function calls.
   static int lastLightValue = 0;   // variable to store the value coming from the sensor
 
-  int thisLightValue = analogRead(LIGHT_SENSOR_PIN);
+  int thisLightValue = analogRead(LIGHT_SENSOR);
 
   if (lastLightValue < LIGHT_THRESHOLD_LOW && lastLightValue != 0) {
     if (thisLightValue > LIGHT_THRESHOLD_HIGH ) {
@@ -155,7 +226,7 @@ void checkButtons() {
   int thisLeftButton   = digitalRead(LEFT_BUTTON);
   int thisRightButton  = digitalRead(RIGHT_BUTTON);
   int thisCenterButton = digitalRead(CENTER_BUTTON);
-  
+
   while (millis() - buttonTimer < BUTTON_DURATION) {
     thisLeftButton   = digitalRead(LEFT_BUTTON);
     thisRightButton  = digitalRead(RIGHT_BUTTON);
@@ -202,31 +273,54 @@ void checkButtons() {
   lastCenterButton = thisCenterButton;
 }
 
-void sendData(int param) {
+/* CheckMessage
+    Gets data from feed once and checks if the message is from the other person.
+*/
+void checkMessages() {
+  bool eventHappened = false;  // Initialize our eventHappened flag to false
+  int eventParameter = 0;       // Change type as needed
 
-  switch (param) {
-    case 1:
-      Serial.println(param);
-      break;
-    case 2:
-      Serial.println(param);
-      break;
-    case 3:
-      Serial.println(param);
-      break;
-    case 4:
-      Serial.println(param);
-      break;
-    case 5:
-      Serial.println(param);
-      break;
-    case 6:
-      Serial.println(param);
-      break;
-    case 7:
-      Serial.println(param);
-      break;
+  // Create variable to hold last message
+  // note: static variables are only initialized once and keep their value between function calls.
+  static String lastMessage = "";   // variable to store the most recent message coming from the feed
+
+  getMessage = GetData();
+  if (lastMessage != getMessage && getMessage.indexOf(your_name) == -1) {
+    eventHappened = true;
   }
+
+  if (eventHappened == true) {
+    Serial.println("New Message received");
+    eventManager.queueEvent(EVENT_MESSAGE, eventParameter);
+  }
+
+  lastMessage = getMessage;
+}
+
+void getPhraseNum(String message) {
+  String testMessage = message.substring(message.indexOf(":"));
+  for (int i = 0; i < 7; i++) {
+    if (messages[i] == testMessage) {
+      phraseNum = i;
+      break;
+    }
+  }
+}
+
+void decodePhrase(int param){
+  bool eventHappened = false;
+  int eventParameter = 0;
+
+  if(param == phraseNum){
+    eventHappened = true;
+  } else {
+    Serial.println("Try again!");
+  }
+
+  if(eventHappened == true){
+    eventManager.queueEvent(EVENT_DECODED, eventParameter);
+  }
+  
 }
 
 void setupWifi() {
@@ -278,23 +372,158 @@ void setupOLED() {
   oled.sendBuffer();
 }
 
+/* GetData
+    Sends a get request to the adafruit feed server that is not from the original user and returns getMessage.
+    @param none
+    @return String of getMessage
+*/
+String GetData() {
+  String output = "";
+
+  // Make sure we're connected to WiFi
+  if (WiFi.status() == WL_CONNECTED) {
+
+    // Create a GET request to the getMessage path
+    GetClient.get(get_io_feed_path);
+    Serial.println("[HTTP] GET... getMessage Requested");
+
+    // read the status code and body of the response
+    int statusCode = GetClient.responseStatusCode();
+    String response = GetClient.responseBody();
+    // CSV string in value,lat,lon,ele format. The lat, lon, and ele values are left blank since they are not set.
+    // i.e. "string",0.0,0.0,0.0
+
+    // Check for the OK from the Server
+    if (statusCode == 200) {
+      Serial.println("[HTTP] GET received reply!");
+
+      String getMessage_string = response.substring(0, response.length() - 4);
+      output = getMessage_string;
+
+    } else if (statusCode > 0) {
+      // Server issue
+      Serial.print("[HTTP] GET... Received response code: ");
+      Serial.println(statusCode);
+    } else {
+      // Client issue
+      Serial.print("[HTTP] GET... Failed, error code: ");
+      Serial.println(statusCode);
+    }
+  } else {
+    Serial.println("[WIFI] Not connected"); // loop to reconnect
+    while (WiFi.status() != WL_CONNECTED) {
+      Serial.print("Attempting to connect to SSID: ");
+      Serial.println(wifi_ssid);
+
+      // Connect to WPA/WPA2 network. SSID and password are set in config.h
+      WiFi.begin(wifi_ssid, wifi_password);
+
+
+      // wait 10 seconds for connection:
+      delay(10000);
+    }
+    digitalWrite(LED_BUILTIN, LOW); // turn off light once connected
+    Serial.println("WiFi successfully connected");
+  }
+
+  return output;
+}
+
+/* PostData
+    Sends a post request to the Adafruit IO server.
+    @param <type> myMessage
+*/
+void PostData(String myMessage) {
+  // Make sure we're connected to WiFi
+  if (WiFi.status() == WL_CONNECTED) {
+
+    Serial.println("[POST] Creating request with value: " + myMessage);
+    PostClient.beginRequest();
+    PostClient.post(post_io_feed_path2);
+    PostClient.sendHeader("X-AIO-Key", io_key2);
+
+    // Tell the server that the type of content we're sending is JSON
+    PostClient.sendHeader("Content-Type", "application/json"); // fill in the header type to specify JSON data
+
+    // Format myMessage to JSON
+    DynamicJsonDocument doc(500);          // create object with arbitrary size 500
+    doc["value"] = myMessage;              // set { "value" : myMessage }
+    String formatted_data;
+    serializeJson(doc, formatted_data);    // save JSON-formatted String to formatted_data
+    PostClient.sendHeader("Content-Length", formatted_data.length()); // fill in the header type to send the length of data
+
+    // Post data, along with headers
+    PostClient.beginBody();
+    PostClient.print(formatted_data);
+    PostClient.endRequest();
+
+    // Handle response from Server
+    int statusCode = PostClient.responseStatusCode();
+    String response = PostClient.responseBody();
+    if (statusCode == 200) {
+      // Response indicated OK
+      Serial.println("[HTTP] POST was successful!");
+    } else if (statusCode > 0) {
+      // Server response received
+      Serial.print("[HTTP] POST... Received response code: ");
+      Serial.println(statusCode);
+    } else {
+      // httpCode will be negative on Client error
+      Serial.print("[HTTP] POST... Failed, error: ");
+      Serial.println(statusCode);
+    }
+  } else {
+    Serial.println("[WIFI] Not connected");
+  }
+}
+
 void stateMachine(int event, int param) {
   SystemState_t nextState = currentState;
   switch (currentState) {
     case INIT:
+      pinMode(LEFT_BUTTON, INPUT);
+      pinMode(RIGHT_BUTTON, INPUT);
+      pinMode(CENTER_BUTTON, INPUT);
+      pinMode(SELECT_BUTTON, INPUT);
+      pinMode(LIGHT_SENSOR, INPUT);
+      buildPhrases();
       nextState = RECEIVE;
       break;
 
     case RECEIVE:
-      if (event == EVENT_BUTTON) {
-        sendData(param);
+      if (event == EVENT_MESSAGE) {
+        Serial.println(getMessage);
+        getPhraseNum(getMessage);
+        //Serial.println("Try decoding message!");
+        //nextState = DECODE;
+      }
+      if (event == EVENT_SELECT) { //select
+        Serial.println("Ready for message input");
+        nextState = SEND;
       }
       break;
 
     case DECODE:
+      if(event == EVENT_BUTTON){
+        decodePhrase(param);
+      }
+      if(event == EVENT_DECODED) {
+        Serial.println("Message Decoded!");
+        Serial.println(getMessage);
+      }
       break;
 
     case SEND:
+      if (event == EVENT_BUTTON) { // confirm sending
+        phrase = messages[param - 1];
+        Serial.print("Message to send: ");
+        Serial.println(phrase);
+      }
+      if (event == EVENT_SELECT) {
+        postMessage = your_name + ": " + phrase;
+        PostData(postMessage);
+        nextState = RECEIVE;
+      }
       break;
 
     default:
